1fc90d3cc17025fe6e71c3ca98e09980b0a78f1e
Your Name <you@example.com>
2025-09-13 19:51:08 +0300

    fix init lib import

--- src/secreton_api_client/__init__.py
+++ src/secreton_api_client/__init__.py
@@ -2,7 +2,7 @@
 
 from ._version import __version__
 from .auth import BearerAuth, SecretOnAuth
-from .client import SecretOnClient
+from .client import AsyncSecretOnClient, SyncSecretOnClient
 from .exceptions import (
     APIClientError,
     AuthenticationError,
@@ -15,7 +15,8 @@ from .exceptions import (
 
 __all__ = [
     "__version__",
-    "SecretOnClient",
+    "AsyncSecretOnClient",
+    "SyncSecretOnClient",
     "BearerAuth",
     "SecretOnAuth",
     "APIClientError",
