0c4ffc202ac75fff4750565de389b446747b00ec
Bezuglyj Vitaly <bevidm@yandex.ru>
2025-11-16 01:25:21 +0300

    Fix docusaurus config

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -11,10 +11,11 @@ const config = {
     favicon: 'img/favicon.ico',
 
     // Set the production url of your site here
-    url: 'https://klukvaofficial.github.io/secreton_api_client_draft/',
+    url: 'https://klukvaofficial.github.io',
     // Set the /<baseUrl>/ pathname under which your site is served
     // For GitHub pages deployment, it is often '/<projectName>/'
     baseUrl: '/secreton_api_client_draft/',
+    trailingSlash: false,
 
     // GitHub pages deployment config.
     // If you aren't using GitHub pages, you don't need these.
